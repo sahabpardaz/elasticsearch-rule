318e4e2a80416d022bea61185d100513754235c2
Mojtaba Zarezadeh <m.zarezadeh@inside.sahab.ir>
2022-08-13 13:56:16 +0430

    Feature: Add support for JUnit 5 with ElasticsearchExtension

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Elasticsearch Rule
+
 [![Tests](https://github.com/sahabpardaz/elasticsearch-rule/actions/workflows/maven.yml/badge.svg?branch=master)](https://github.com/sahabpardaz/elasticsearch-rule/actions/workflows/maven.yml)
 [![Coverage](https://sonarcloud.io/api/project_badges/measure?project=sahabpardaz_elasticsearch-rule&metric=coverage)](https://sonarcloud.io/dashboard?id=sahabpardaz_elasticsearch-rule)
 [![Duplicated Lines (%)](https://sonarcloud.io/api/project_badges/measure?project=sahabpardaz_elasticsearch-rule&metric=duplicated_lines_density)](https://sonarcloud.io/dashboard?id=sahabpardaz_elasticsearch-rule)
@@ -33,7 +34,9 @@ public void testClient() {
     Assert.assertTrue(createIndexResponse.isAcknowledged());
 }
 ```
+
 It is also possible to get the network address of the Elasticsearch server and construct the TransportClient:
+
 ```java
 @BeforeClass
 public static void setUpClass() {
@@ -52,6 +55,35 @@ public static void setUpClass() {
 }
 ```
 
+## JUnit 5
+
+Sample usage:
+
+```java
+private static final String ELASTICSEARCH_CLUSTER_NAME="elasticsearch";
+
+@RegisterExtension
+static ElasticsearchExtension elasticsearchExtension=new ElasticsearchExtension(ELASTICSEARCH_CLUSTER_NAME);
+private static TransportClient transportClient;
+
+@BeforeAll
+static void setUpClass(){
+        transportClient=elasticsearchExtension.getTransportClient();
+}
+
+@Test
+public void testClient(){
+        String indexName="twitter";
+        CreateIndexResponse createIndexResponse=transportClient.admin().indices().prepareCreate(indexName).get();
+        Assert.assertTrue(createIndexResponse.isAcknowledged());
+}
+```
+
 ## Add it to your project
-You can refer to this library by either of java build systems (Maven, Gradle, SBT or Leiningen) using snippets from this jitpack link:
+
+You can refer to this library by either of java build systems (Maven, Gradle, SBT or Leiningen) using snippets from this
+jitpack link:
 [![](https://jitpack.io/v/sahabpardaz/elasticsearch-rule.svg)](https://jitpack.io/#sahabpardaz/elasticsearch-rule)
+
+JUnit 4 and 5 dependencies are marked as optional, so you need to provide JUnit 4 or 5 dependency
+(based on what version you need, and you use) in you project to make it work.
--- src/main/java/ir/sahab/elasticsearchrule/ElasticsearchExtension.java
+++ src/main/java/ir/sahab/elasticsearchrule/ElasticsearchExtension.java
@@ -1,14 +1,6 @@
 package ir.sahab.elasticsearchrule;
 
 import ir.sahab.cleanup.Cleanups;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.concurrent.ExecutionException;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
 import org.elasticsearch.action.admin.indices.template.put.PutIndexTemplateRequest;
 import org.elasticsearch.action.admin.indices.template.put.PutIndexTemplateResponse;
@@ -23,40 +15,63 @@ import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.reindex.ReindexPlugin;
 import org.elasticsearch.node.InternalSettingsPreparer;
 import org.elasticsearch.node.Node;
-import org.elasticsearch.node.NodeValidationException;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.transport.Netty4Plugin;
 import org.elasticsearch.transport.TransportService;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
-import org.junit.rules.ExternalResource;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
- * A JUnit 4 {@link org.junit.rules.TestRule} for starting an elasticsearch server on the local machine.
+ * A JUnit 5 Extension for starting an elasticsearch server instance on the local machine.
  */
-public class ElasticsearchRule extends ExternalResource {
-
+public class ElasticsearchExtension implements BeforeAllCallback, AfterAllCallback {
     private final String clusterName;
-
-    private Path ruleTempDirectory;
+    private Path tempDirectory;
     private TransportClient transportClient;
     private TransportAddress transportAddress;
     private Node server;
 
-    public ElasticsearchRule(String clusterName) {
+    public ElasticsearchExtension(String clusterName) {
         this.clusterName = clusterName;
     }
 
+
     @Override
-    protected void before() throws IOException, NodeValidationException, ExecutionException, InterruptedException {
-        ruleTempDirectory = Files.createTempDirectory("elasticsearch-junit-rule");
+    public void afterAll(ExtensionContext context) {
+        try {
+            Cleanups.of(transportClient, server).and(() -> Files.walk(tempDirectory).sorted(Comparator.reverseOrder()).forEach(path -> {
+                try {
+                    Files.deleteIfExists(path);
+                } catch (IOException e) {
+                    throw new AssertionError("Unable to remove temporary file: " + path, e);
+                }
+            })).doAll();
+        } catch (IOException e) {
+            throw new AssertionError("Unable to close resources", e);
+        }
+    }
+
+    @Override
+    public void beforeAll(ExtensionContext context) throws Exception {
+        tempDirectory = Files.createTempDirectory("elasticsearch-junit-extension");
 
         // Set up a setting for Elasticsearch server node.
         Settings.Builder builder = Settings.builder();
         builder.put(NetworkModule.TRANSPORT_TYPE_KEY, Netty4Plugin.NETTY_TRANSPORT_NAME);
         builder.put("node.id.seed", 0L);
         builder.put("node.name", "node1");
-        builder.put(Environment.PATH_DATA_SETTING.getKey(), ruleTempDirectory.resolve("elastic-data"));
-        builder.put(Environment.PATH_HOME_SETTING.getKey(), ruleTempDirectory.resolve("elastic-home"));
+        builder.put(Environment.PATH_DATA_SETTING.getKey(), tempDirectory.resolve("elastic-data"));
+        builder.put(Environment.PATH_HOME_SETTING.getKey(), tempDirectory.resolve("elastic-home"));
         builder.put(ClusterName.CLUSTER_NAME_SETTING.getKey(), clusterName);
         builder.put("discovery.type", "single-node");
         Settings settings = builder.build();
@@ -66,8 +81,7 @@ public class ElasticsearchRule extends ExternalResource {
         // ReindexPlugin is necessary for making "delete by query" available.
         server = new TestNode(settings, Arrays.asList(Netty4Plugin.class, ReindexPlugin.class));
         server.start();
-        ClusterHealthResponse clusterHealthResponse = server.client().admin().cluster().prepareHealth()
-                .setWaitForGreenStatus().get();
+        ClusterHealthResponse clusterHealthResponse = server.client().admin().cluster().prepareHealth().setWaitForGreenStatus().get();
         if (clusterHealthResponse.getStatus() != ClusterHealthStatus.GREEN) {
             throw new AssertionError("The state of the cluster did not change to green.");
         }
@@ -78,48 +92,15 @@ public class ElasticsearchRule extends ExternalResource {
         transportClient.addTransportAddress(transportAddress);
 
         // By default, every index that is created has 5 shards and 1 replica.
-        // However, the rule provides only a single node cluster. In order to change them,
+        // However, this extension provides only a single node cluster. In order to change them,
         // a template is created that is used by default for all indexes created.
-        PutIndexTemplateRequest request = new PutIndexTemplateRequest("default-junit-rule-template");
+        PutIndexTemplateRequest request = new PutIndexTemplateRequest("default-junit-extension-template");
         request.patterns(Collections.singletonList("*"));
         request.order(-1);
-        request.settings(Settings.builder()
-                .put("index.number_of_shards", 1)
-                .put("index.number_of_replicas", 0)
-        );
+        request.settings(Settings.builder().put("index.number_of_shards", 1).put("index.number_of_replicas", 0));
         PutIndexTemplateResponse putTemplateResponse = transportClient.admin().indices().putTemplate(request).get();
         if (!putTemplateResponse.isAcknowledged()) {
             throw new AssertionError("Adding the default template has encountered an error.");
-        }
-    }
-
-    @Override
-    protected void after() {
-        try {
-            Cleanups.of(transportClient, server)
-                    .and(() -> Files.walk(ruleTempDirectory)
-                            .sorted(Comparator.reverseOrder())
-                            .forEach(path -> {
-                                try {
-                                    Files.deleteIfExists(path);
-                                } catch (IOException e) {
-                                    throw new AssertionError("Unable to remove temporary file: " + path, e);
-                                }
-                            }))
-                    .doAll();
-        } catch (IOException e) {
-            throw new AssertionError("Unable to close resources", e);
-        }
-    }
-
-    /**
-     * A wrapper class for class org.elasticsearch.node.Node to make its constructor public.
-     */
-    public static class TestNode extends Node {
-
-        public TestNode(Settings preparedSettings, Collection<Class<? extends Plugin>> classpathPlugins) {
-            super(InternalSettingsPreparer.prepareEnvironment(preparedSettings, Terminal.DEFAULT,
-                    Collections.emptyMap(), null), classpathPlugins);
         }
     }
 
@@ -132,10 +113,16 @@ public class ElasticsearchRule extends ExternalResource {
     }
 
     public void waitForGreenStatus() {
-        ClusterHealthResponse clusterHealthResponse = transportClient.admin().cluster().prepareHealth()
-                .setWaitForGreenStatus().get();
+        ClusterHealthResponse clusterHealthResponse = transportClient.admin().cluster().prepareHealth().setWaitForGreenStatus().get();
         if (clusterHealthResponse.getStatus() != ClusterHealthStatus.GREEN) {
             throw new AssertionError("The state of the cluster did not change to green.");
+        }
+    }
+
+    static class TestNode extends Node {
+
+        public TestNode(Settings preparedSettings, Collection<Class<? extends Plugin>> classpathPlugins) {
+            super(InternalSettingsPreparer.prepareEnvironment(preparedSettings, Terminal.DEFAULT, Collections.emptyMap(), null), classpathPlugins);
         }
     }
 }
